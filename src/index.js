2a604e7a060ab3e494fc7ac402406489663fec2c
Gonzo <gvidal.industrial@gmail.com>
2021-11-16 15:08:15 -0500

    Fetch data from avo api and create 3 elements

--- src/index.js
+++ src/index.js
@@ -3,4 +3,25 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = "https://platzi-avo.vercel.app/api/avo";
+
+//web api
+window
+  .fetch(url)
+  // Conect to server
+  .then(response => response.json())
+  // process response and convert into JSON
+  .then((responseJson) => {
+    responseJson.data.forEach((item) => {
+      // Create Image
+      const image = document.createElement("img");
+      document.body.appendChild(image);
+      // Create Title
+      const title = document.createElement("h2");
+      document.body.appendChild(title);
+      // Create price
+      const price = document.createElement("div");
+      document.body.appendChild(price);
+  })
+  })
+  // JSON => Data => Web browser info rendering
