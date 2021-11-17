360186eacc9495974bd2b8fffed4fcf895a3e48a
Gonzo <gvidal.industrial@gmail.com>
2021-11-17 09:39:30 -0500

    Add styles

--- src/index.js
+++ src/index.js
@@ -3,25 +3,56 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-const url = "https://platzi-avo.vercel.app/api/avo";
+const BASE_URL = "https://platzi-avo.vercel.app"
+const url = `${BASE_URL}/api/avo`;
 
+const formatPrice = (price) => {
+  const newPrice = new window.Intl.NumberFormat("en-EN", {
+    style: "currency",
+    currency: "USD",
+  }).format(price)
+  return newPrice;
+}
+
+const appNode = document.querySelector("#app");
 //web api
-window
-  .fetch(url)
-  // Conect to server
-  .then(response => response.json())
-  // process response and convert into JSON
-  .then((responseJson) => {
-    responseJson.data.forEach((item) => {
-      // Create Image
-      const image = document.createElement("img");
-      document.body.appendChild(image);
-      // Create Title
-      const title = document.createElement("h2");
-      document.body.appendChild(title);
-      // Create price
-      const price = document.createElement("div");
-      document.body.appendChild(price);
+// Conect to server with async await
+async function myFetch(){
+  const r1 = await window.fetch(url);
+  const r2 = await r1.json();
+
+  const totalItems = [];
+
+  r2.data.forEach((item) => {
+    // Create Image
+    const image = document.createElement("img");
+    image.setAttribute("src", `${BASE_URL}${item.image}` );
+
+    // Create Title
+    const title = document.createElement("h2");
+    title.className = "text-lg";
+    title.textContent = item.name;
+
+    // Create price
+    const price = document.createElement("div");
+    price.className = "text-gray-600";
+    price.textContent = formatPrice(item.price);
+
+    // Wrap price & title
+    const priceAndTitle = document.createElement("div");
+    priceAndTitle.className = "text-center md:text-left";
+    priceAndTitle.append(title, price);
+
+    // Wrap Img and priceAndTitle
+    const card = document.createElement("div");
+    card.className = "card md:flex bg-white rounded-lg p-6 hover:bg-gray-100";
+    card.classList.add("shadow-lg");
+    card.append(image, priceAndTitle);
+    
+    totalItems.push(card);
+
   })
-  })
-  // JSON => Data => Web browser info rendering
+  appNode.append(...totalItems);
+}
+
+myFetch();
